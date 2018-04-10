6c78339f1e1cbcaae24421f6420b88e94b69eb8b
Alexandre Moine <alexandre@moine.me>
2018-04-10 11:47:27 +0200

    Add simple graph consumers

--- bench/BenchAlga.hs
+++ bench/BenchAlga.hs
@@ -17,6 +17,12 @@ instance GraphImpl (Graph Int) where
 isEmpty' :: ToFuncToBench (Graph Int)
 isEmpty' = const $ Consummer "IsEmpty" isEmpty
 
+vertexCount' :: ToFuncToBench (Graph Int)
+vertexCount' = const $ Consummer "vertexCount" vertexCount
+
+edgeCount' :: ToFuncToBench (Graph Int)
+edgeCount' = const $ Consummer "edgeCount" edgeCount
+
 --A simple function
 pathHasEdge :: ToFuncToBench (Graph Int)
 pathHasEdge = FuncWithArg "hasEdge" (uncurry hasEdge) show . take 2 . edgesNotInPath
@@ -26,5 +32,5 @@ tenPowers = 1: map (10*) tenPowers
 
 main :: IO ()
 main = do
-  let toTest = map benchFunc [isEmpty', pathHasEdge]
+  let toTest = map benchFunc [isEmpty', vertexCount', edgeCount', pathHasEdge]
   defaultMain $ toTest <*> (map mkPath $ take 5 tenPowers)
