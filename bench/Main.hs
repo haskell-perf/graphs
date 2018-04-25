c21f090b4a602dba5f959d6dbcf8e46ceec75dc8
Alexandre Moine <alexandre@moine.me>
2018-04-25 14:55:58 +0200

    Comments

--- bench/Main.hs
+++ bench/Main.hs
@@ -2,17 +2,12 @@ import Data.List (sortBy, filter, nubBy)
 import Data.Maybe (mapMaybe)
 
 import Control.Monad (unless)
-import Control.Monad.Except (runExceptT)
-
 import Criterion
 import Criterion.Types
-import Criterion.Internal -- (runOne)
+import Criterion.Internal
 import Criterion.Main.Options (defaultConfig)
 import Criterion.Measurement (initializeTime)
-import Criterion.IO.Printf (printError)
-import Criterion.Analysis (analyseSample)
-import Criterion.Monad (withConfig, Criterion)
-import qualified Data.Vector as V
+import Criterion.Monad (withConfig)
 
 import Statistics.Types
 
@@ -32,7 +27,11 @@ genReport todo = do
   putStrLn "# Compare benchmarks" 
   genReport' 2 todo
 
-genReport' :: Int -> [(String,Benchmark)] -> IO()
+genReport' :: Int
+           -- ^ The number of '#' to write
+           -> [(String,Benchmark)]
+           -- ^ THe list of benchmarks with their library name
+           -> IO()
 genReport' _ [] = putStrLn "\nNo data\n"
 genReport' lev arr = mapM_ toPrint $ nubBy (\(_,a) (_,b) -> a == b) arr
   where
@@ -46,18 +45,20 @@ genReport' lev arr = mapM_ toPrint $ nubBy (\(_,a) (_,b) -> a == b) arr
           BenchGroup{} -> genReport' (lev+1) $ concatMap tkChilds $ here breport
     here e = filter (\(_,b) -> e==b) arr
 
+-- | Bench only if it is possible
 tkSimple :: (String, Benchmark) -> Maybe (IO (String, Double))
 tkSimple (libName,Benchmark _ b) = Just $ benchmarkWithoutOutput b >>= \x -> return (libName, getMean x)
 tkSimple (_,BenchGroup{}) = Nothing
+
+-- | Get the childs of a BenchGroup, inserting the name of the library
+tkChilds :: (String,Benchmark) -> [(String,Benchmark)]
+tkChilds (_,Benchmark{}) = []
+tkChilds (lib,BenchGroup _ childs) = insertName lib childs
 
 showSimples :: [(String,Double)] -> String
 showSimples = unlines . map shw . sortBy (\(_,t1) (_,t2) -> t1 `compare` t2)
   where
     shw (libname, time) = "* " ++ libname ++ " : " ++ show time ++ " s. (Mean)"
-
-tkChilds :: (String,Benchmark) -> [(String,Benchmark)]
-tkChilds (_,Benchmark{}) = []
-tkChilds (lib,BenchGroup _ childs) = insertName lib childs
 
 getMean :: Report -> Double
 getMean = estPoint . anMean . reportAnalysis
@@ -70,9 +71,9 @@ benchmarkWithoutOutput bm = do
     return rpt
   where
     defaultConfig' = defaultConfig {verbosity = Quiet}
-    
-main :: IO ()
-main = genReport $ concatMap (uncurry insertName) [("Alga",Alga.allBenchs), ("Containers",Containers.allBenchs), ("Fgl",Fgl.allBenchs)]
 
 insertName :: String -> [i] -> [(String,i)]
 insertName name = map (\x -> (name, x))
+
+main :: IO ()
+main = genReport $ concatMap (uncurry insertName) [("Alga",Alga.allBenchs), ("Containers",Containers.allBenchs), ("Fgl",Fgl.allBenchs)]
