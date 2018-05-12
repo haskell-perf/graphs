0976358df51d677257be722dab51f9c524915c3e
Alexandre Moine <alexandre@moine.me>
2018-05-12 18:15:25 +0200

    Named

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,9 +1,6 @@
-{-# LANGUAGE TupleSections #-}
-
-import Data.List (sortBy, filter, nubBy, uncons)
+import Data.List (sortBy, filter, nub, sort)
 import Data.Maybe (mapMaybe, isNothing, isJust)
 import Control.Monad (unless, void, when)
-import System.Environment (getArgs)
 import Data.Map.Strict (Map, alter, unionWith, empty, toList)
 
 import Criterion
@@ -21,19 +18,17 @@ import qualified Fgl.PatriciaTree
 import qualified HashGraph.Gr
 
 import BenchGraph (allBenchs, allWeighs)
+import BenchGraph.Named
 
 import Options.Applicative (execParser)
 import Command
 
-showBenchmark :: Benchmark -> String
-showBenchmark (Benchmark name _) = name
-showBenchmark (BenchGroup name _) = name
+instance WithName Benchmark where
+  getName (Benchmark name _) = name
+  getName (BenchGroup name _) = name
 
 instance Eq Benchmark where
-  a == b = showBenchmark a == showBenchmark b
-
-eq :: (a, Benchmark) -> (b, Benchmark) -> Bool
-eq (_,a) (_,b) = a==b
+  a == b = getName a == getName b
 
 comparesS :: (Ord a) => (b,a) -> (c,a) -> Ordering
 comparesS (_,t1) (_,t2) = t1 `compare` t2
@@ -44,25 +39,24 @@ genReport :: Int
            -- ^ The number of '#' to write
            -> Maybe Flag
            -- ^ Flag ?
-           -> [(String,Benchmark)]
+           -> [Named Benchmark]
            -- ^ The list of benchmarks with their library name
            -> IO()
 genReport _ _ [] = putStrLn "\nNo data\n"
-genReport lev flg arr = mapM_ (\x -> toPrint lev flg arr (snd x) >>= printMap . getFastest empty) $ nubBy eq arr
+genReport lev flg arr = mapM_ (\x -> toPrint lev flg arr (obj x) >>= printMap . getFastest empty) $ nub arr
 
-toPrint :: Int -> Maybe Flag -> [(String,Benchmark)] -> Benchmark -> IO (Grouped [(String, Double)])
+toPrint :: Int -> Maybe Flag -> [Named Benchmark] -> Benchmark -> IO (Grouped [Named Double])
 toPrint lev flg arr breport = do
-  let name = showBenchmark breport
-  unless (null name || (isJust flg && lev /= 2)) $ putStrLn $ replicate lev '#' ++ " " ++ showBenchmark breport
+  unless (null (getName breport) || (isJust flg && lev /= 2)) $ putStrLn $ replicate lev '#' ++ " " ++ getName breport
   case breport of
     Benchmark{} -> do
       simples <- sequence $ mapMaybe tkSimple $ here breport
       when (isNothing flg) $ putStrLn $ "\n" ++ showSimples simples
       return $ Simple simples
-    BenchGroup{} -> Group <$> mapM (toPrint (lev+1) flg otherGroups . snd) (nubBy eq otherGroups)
+    BenchGroup{} -> Group <$> mapM (toPrint (lev+1) flg otherGroups) (nub $ map obj otherGroups)
   where
     otherGroups = concatMap tkChilds $ here breport
-    here e = filter (\(_,b) -> e==b) arr
+    here e = filter (\(Named _ x) -> e==x) arr
 
 printMap :: Map String Int -> IO ()
 printMap m = do
@@ -70,27 +64,27 @@ printMap m = do
   void $ foldMap (\(k,v) -> putStrLn $ k ++" was the fastest " ++show v++" times") $ sortBy (flip comparesS) $ toList m
   putStrLn ""
 
-getFastest :: Map String Int -> Grouped [(String,Double)] -> Map String Int
-getFastest m (Simple a) = getFastest' (sortBy comparesS a) m
+getFastest :: Map String Int -> Grouped [Named Double] -> Map String Int
+getFastest m (Simple a) = getFastest' (sort a) m
 getFastest m (Group grp) = foldr (unionWith (+) . getFastest m) empty grp
 
-getFastest' :: [(String,Double)] -> Map String Int -> Map String Int
-getFastest' (l:_) = alter (Just . maybe 1 (+ 1)) $ fst l
+getFastest' :: [Named Double] -> Map String Int -> Map String Int
+getFastest' (l:_) = alter (Just . maybe 1 (+ 1)) $ show l
 
 -- | Bench only if it is possible
-tkSimple :: (String, Benchmark) -> Maybe (IO (String, Double))
-tkSimple (libName,Benchmark _ b) = Just $ benchmarkWithoutOutput b >>= \x -> return (libName, getMean x)
-tkSimple (_,BenchGroup{}) = Nothing
+tkSimple :: Named Benchmark -> Maybe (IO (Named Double))
+tkSimple (Named libName (Benchmark _ b)) = Just $ Named libName . getMean <$> benchmarkWithoutOutput b
+tkSimple (Named _ BenchGroup{}) = Nothing
 
 -- | Get the childs of a BenchGroup, inserting the name of the library
-tkChilds :: (String,Benchmark) -> [(String,Benchmark)]
-tkChilds (_,Benchmark{}) = []
-tkChilds (lib,BenchGroup _ childs) = insertName lib childs
+tkChilds :: Named Benchmark -> [Named Benchmark]
+tkChilds (Named _ Benchmark{}) = []
+tkChilds (Named lib (BenchGroup _ childs)) = nameChilds lib childs
 
-showSimples :: [(String,Double)] -> String
-showSimples = unlines . map shw . sortBy comparesS
+showSimples :: [Named Double] -> String
+showSimples = unlines . map shw . sort
   where
-    shw (libname, time) = "* " ++ libname ++ " : " ++ show time ++ " s. (Mean)"
+    shw (Named n o) = "* " ++ n ++ " : " ++ show o ++ " s. (Mean)"
 
 getMean :: Report -> Double
 getMean = estPoint . anMean . reportAnalysis
@@ -104,14 +98,8 @@ benchmarkWithoutOutput bm = do
   where
     defaultConfig' = defaultConfig {verbosity = Quiet}
 
-insertName :: String -> [i] -> [(String,i)]
-insertName name = map (name,)
-
-showBenchsName :: [(a,Benchmark)] -> String
-showBenchsName = unlines . map (showBenchmark . snd)
-
-elemBy :: (a -> a -> Bool) -> a -> [a] -> Bool
-elemBy f a = foldr (\x y -> f a x || y) False
+nameChilds :: String -> [i] -> [Named i]
+nameChilds name = map (nameBy $ const name)
 
 main :: IO ()
 main = execParser commandI >>= main'
@@ -119,25 +107,25 @@ main = execParser commandI >>= main'
 main' :: Command -> IO ()
 main' opts
   = case opts of
-      List -> putStr $ showBenchsName grList'
+      List -> putStr $ showListN grList'
       Run opt' flg -> do
           let todo = case opt' of
                 Nothing -> grList
                 Just opt'' -> case opt'' of
-                  Only name -> filter ((==) name . showBenchmark . snd) grList
+                  Only name -> filter ((==) name . getName) grList
                   Part one' two -> let one = one' + 1
                                        per = length grList' `div` two
                                    in drop ((one-1)*per) $ take (one*per) grList'
-          let samples = filter ((flip $ elemBy eq) todo) grList
+          let samples = filter (`elem` todo) grList
           putStrLn "# Compare benchmarks\n"
           putStrLn "Doing:"
-          putStrLn $ "\n----\n"++showBenchsName (nubBy eq samples) ++ "----\n"
+          putStrLn $ "\n----\n"++ showListN (nub samples) ++ "----\n"
           genReport 2 flg todo
 
   where
-    grList = concatMap (uncurry insertName) [
+    grList = concatMap (uncurry nameChilds) [
      ("Alga (Algebra.Graph)",allBenchs Alga.Graph.functions),
      ("Containers (Data.Graph)",allBenchs Containers.Graph.functions),
      ("Fgl (Data.Graph.Inductive.PatriciaTree)", allBenchs Fgl.PatriciaTree.functions),
      ("Hash-Graph (Data.HashGraph.Strict)", allBenchs HashGraph.Gr.functions)]
-    grList' = nubBy eq grList
+    grList' = nub grList
---
+++ src/BenchGraph/Named.hs
@@ -0,0 +1,49 @@
+module BenchGraph.Named
+  (
+    Name,
+    Named (..),
+    WithName,
+    getName,
+    nameShow,
+    nameBy,
+    toNamed,
+    showListN
+  )
+where
+
+type Name = String
+
+class WithName a where
+  getName :: a -> String
+
+showListN :: WithName a => [a] -> String
+showListN = unlines . map getName
+
+data Named a = Named {
+  name :: Name,
+  obj :: a
+                     }
+
+instance WithName (Named a) where
+  getName = name
+
+instance Eq (Named a) where
+  (Named a _ ) == (Named b _) = a == b
+
+instance Show (Named a) where
+  show (Named a _) = a
+
+instance Ord a => Ord (Named a) where
+  a <= b = obj a <= obj b
+
+instance Functor Named where
+  fmap f (Named n x) = Named n (f x)
+
+nameShow :: Show a => a -> Named a
+nameShow = nameBy show
+
+nameBy :: (a -> String) -> a -> Named a
+nameBy f a = Named (f a) a
+
+toNamed :: (String,a) -> Named a
+toNamed = uncurry Named
