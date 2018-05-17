d703498ffff23105bb7173d5a9cf2954564f34ae
Alexandre Moine <alexandre@moine.me>
2018-05-17 17:13:20 +0200

    COmments on Named

--- src/BenchGraph/Named.hs
+++ src/BenchGraph/Named.hs
@@ -16,24 +16,29 @@ import Data.Functor.Classes
 
 type Name = String
 
+-- | The Named data, a Name and the data
 data Named a = Named Name a
 
+-- | Show is defined by the name of the object
 instance Show (Named a) where
   show (Named name _) = name
 
 instance Functor Named where
   fmap = liftW
 
+-- | Named is a CoMonad
 instance Comonad Named where
   extract (Named _ obj) = obj
   extend f named = Named (show named) $ f named
 
 instance Foldable Named where
-  foldMap f x = f $ extract x
+  foldMap = liftExtract
 
+-- | You can sequence a name
 instance Traversable Named where
   sequenceA (Named n m) = Named n <$> m
 
+-- | Named are equals if their data are
 instance Eq a => Eq (Named a) where
   (==) = liftExtract2 (==)
 
@@ -52,7 +57,8 @@ toNamed = uncurry Named
 fromNamed :: Named a -> (Name,a)
 fromNamed (Named n a) = (n,a)
 
-liftExtract :: (a -> b) -> Named a -> b
+-- | Lifting functions in comonads
+liftExtract :: (Comonad w) => (a -> b) -> w a -> b
 liftExtract f = f . extract
 
 liftExtract2 :: (Comonad w) => (a-> b -> c) -> w a -> w b -> c
