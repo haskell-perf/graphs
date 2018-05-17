cd12e76538a09e3d326dc6e5b50109135489ccd7
Alexandre Moine <alexandre@moine.me>
2018-05-17 15:26:19 +0200

    Cleanup

--- bench/Space.hs
+++ bench/Space.hs
@@ -3,15 +3,6 @@ import Data.Maybe (mapMaybe, catMaybes, isJust, isNothing)
 import Data.Int (Int64)
 import Control.Monad (unless, when, (>=>))
 
-import qualified Alga.Graph
-import qualified Containers.Graph
-import qualified Fgl.PatriciaTree
-import qualified HashGraph.Gr
-
-import BenchGraph (allWeighs)
-import BenchGraph.Named
-import BenchGraph.Utils (mainWeigh)
-
 import Control.Comonad (extract)
 
 import Weigh
@@ -24,6 +15,15 @@ import Options.Applicative (execParser)
 import Command
 import qualified Types as Ty
 import Best
+
+import BenchGraph (allWeighs)
+import BenchGraph.Named
+import BenchGraph.Utils (mainWeigh)
+
+import qualified Alga.Graph
+import qualified Containers.Graph
+import qualified Fgl.PatriciaTree
+import qualified HashGraph.Gr
 
 showGrouped :: Grouped a -> String
 showGrouped (Grouped n _) = n
@@ -40,21 +40,15 @@ takeLastAfterBk w = case elemIndices '/' w of
                           [] -> w
                           x -> drop (1+last x) w
 
--- | Name from grouped, necessary for the first level of Grouped for Weigh
-groupedToNamed :: Grouped a -> Maybe (Named [Grouped a])
-groupedToNamed (Grouped n rst) = Just $ Named n rst
-groupedToNamed _ = Nothing
-
 useResults :: Maybe Flag -> [Grouped (Weight, Maybe String)] -> IO ()
 useResults flg res = mapM_ ((printReport 2 flg namedBenchs . extract) >=> maybe (return ()) (printBest "used the least amount of memory")) benchs'
   where
     namedBenchs = concatMap sequence $ mapMaybe groupedToNamed res
     benchs' = nubBy (liftExtract2 eqG) namedBenchs
 
-
 -- | Print a report from the lists of benchmarks
 printReport :: Int -- ^ The number of # to write
-            -> Maybe Flag -- ^ Maybe a flag
+            -> Maybe Flag -- ^ Maybe a flag, it will desactivate all output except the sumarry
             -> [Named (Grouped (Weight, Maybe String))] -- ^ The list of benchs
             -> Grouped (Weight, Maybe String) -- ^ A selected bench name
             -> IO (Maybe (Ty.Grouped [Named Int64])) -- Maybe if there was actual data
@@ -64,12 +58,14 @@ printReport lev flg arr act = do
   case act of
     (Grouped _ (Singleton{}:_)) -> Just . Ty.Group <$> mapM (printSimples (lev+1) flg semiSimples . extract) (nubBy (liftExtract2 eqW) semiSimples)
     Grouped{} -> case otherGroups of
-                   [] -> when (isNothing flg) (putStrLn "No data\n") >> return Nothing
+                   [] -> do
+                     when (isNothing flg) $ putStrLn "No data\n"
+                     return Nothing
                    real -> Just . Ty.Group . catMaybes <$> mapM (printReport (lev+1) flg otherGroups . extract) (nubBy (liftExtract2 eqG) real)
     where
-      semiSimples = mapMaybe (traverse tkSingl) otherGroups
-      otherGroups = concatMap sequence $ mapMaybe (traverse tkChilds) $ here act
       here e = filter (eqG e . extract) arr
+      otherGroups = concatMap sequence $ mapMaybe (traverse tkChilds) $ here act
+      semiSimples = mapMaybe (traverse tkSingl) otherGroups
 
 -- | Really print the simples, different than printReport for type reason
 printSimples :: Int -> Maybe Flag -> [Named (Weight, Maybe String)] -> (Weight, Maybe String) -> IO (Ty.Grouped [Named Int64])
@@ -81,12 +77,10 @@ printSimples lev flg arr act = do
   where
     -- filter by the 'act' argument, and sort
     filtered = sortBy (liftExtract2 $ \(x,_) (y,_) -> weightAllocatedBytes x `compare` weightAllocatedBytes y) $ filter (liftExtract (eqW act)) arr
-    filtered' = map extract filtered
-    libs = map show filtered
     table = T.Table
-      (T.Group T.NoLine $ map T.Header libs)
+      (T.Group T.NoLine $ map (T.Header . show) filtered)
       (T.Group T.SingleLine [T.Header "AllocatedBytes", T.Header "GCs"])
-      (map (showWeight . fst) filtered')
+      (map (showWeight . fst . extract) filtered)
 
 -- | Convert a @Weight@ to a list of @String@ for tabular representation
 showWeight :: Weight -> [String]
@@ -97,10 +91,14 @@ tkSingl :: Grouped (Weight, Maybe String) -> Maybe (Weight, Maybe String)
 tkSingl (Singleton b) = Just b
 tkSingl _ = Nothing
 
+-- | Name from grouped, necessary for the first level of Grouped for Weigh
+groupedToNamed :: Grouped a -> Maybe (Named [Grouped a])
+groupedToNamed (Grouped n rst) = Just $ Named n rst
+groupedToNamed _ = Nothing
+
 -- | Get the childs of a BenchGroup, inserting the name of the library
 tkChilds :: Grouped (Weight, Maybe String) -> Maybe [Grouped (Weight, Maybe String)]
-tkChilds (Grouped _ childs) = Just childs
-tkChilds _ = Nothing
+tkChilds = groupedToNamed >=> Just . extract
 
 main :: IO ()
 main = execParser flagSpace >>= main'
