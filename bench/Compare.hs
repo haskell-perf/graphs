078ae8af4be41f7d05ebd08678d6506731cbfc73
Alexandre Moine <alexandre@moine.me>
2018-05-22 23:01:33 +0200

    Print the lib in order, and always compare to the worst

--- bench/Compare.hs
+++ bench/Compare.hs
@@ -7,26 +7,40 @@ where
 
 import Data.Map.Strict (Map, unionWith, empty, toList, fromList)
 import qualified Data.Map.Strict as M
-import Data.List (find)
+import Data.List (find, sort)
+import Text.Printf (printf)
 
 import BenchGraph.Named
 
 import Types
 
 compareResults :: Grouped [Named Double] -> IO ()
-compareResults = printMap . getComparison
+compareResults = printMap . fmap reverse . rearrange . getComparison
 
-printMap :: (String,[Named [Double]]) -> IO ()
-printMap (ref,res) = mapM_ (\(Named name arr) -> putStrLn $ unwords ["*",name,"was",show $ average arr,"times faster than",ref]) res
+-- | Sort the results, plus put the worst lib for comparison
+rearrange :: Named [Named Double] -> Named [Named Double]
+rearrange (Named n arr) =
+  if db >= 1
+     then Named n sorted
+     else rearrange $ Named n' $ Named n (recip db) : map (fmap (recip db *)) (tail sorted)
+  where
+    sorted = sort arr
+    (Named n' db) = head sorted
+
+printMap :: Named [Named Double] -> IO ()
+printMap (Named ref res) = do
+  putStrLn "\nSUMMARY:\n"
+  mapM_ (\(Named name av) -> putStrLn $ unwords [" *",name,"was",printf "%.2f" av,"times faster than",ref]) res
+  putStrLn ""
 
 average :: Fractional a => [a] -> a
 average lst = sum lst / fromRational (toRational (length lst))
 
-getComparison :: Grouped [Named Double] -> (String,[Named [Double]])
-getComparison grp = (ref grp, map toNamed $ toList $ getComparison' (ref grp) start grp)
+getComparison :: Grouped [Named Double] -> Named [Named Double]
+getComparison grp = Named (ref grp) $ map toNamed $ toList $ M.map average $ getComparison' (ref grp) start grp
   where
     ref = show . head . getRef
-    getRef grp = case grp of
+    getRef grp' = case grp' of
                    (Simple a) -> a
                    (Group (x:_)) -> getRef x
     start = fromList $ map (\x -> (show x, [])) $ tail $ getRef grp
