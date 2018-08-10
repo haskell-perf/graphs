3912ea6d9606a060c45a9cf2d90c5c6dd01264a8
Alexandre Moine <alexandre@moine.me>
2018-08-10 20:17:07 +0200

    Even better travis

---
+++ travis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+stack --no-terminal bench --no-run-benchmarks --flag "bench-graph:-reallife"
+
+PREFIX=.stack-work/dist/x86_64-linux/Cabal-2.2.0.1/build
+
+if [ $DO = SPACE ]; then travis_wait 30 $PREFIX/space/space; else $PREFIX/time/time run --part $STEP --of 3  -g '("Mesh",2)' -g '("Clique",2)' ; fi;
